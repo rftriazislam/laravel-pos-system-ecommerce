f8c72c178cb7bf4cfc5cc69444d11c15784b1121
rftriazislam <riazislam0999@gmail.com>
2021-08-20 22:46:57 +0600

    sales updates and modal ( delete_modal blade modified)

--- resources/views/modals/delete_modal.blade.php
+++ resources/views/modals/delete_modal.blade.php
@@ -4,11 +4,11 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title h6">{{translate('Delete Confirmation')}}</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body text-center">
                 <p class="mt-1">{{translate('Are you sure to delete this?')}}</p>
-                <button type="button" class="btn btn-link mt-2" data-dismiss="modal">{{translate('Cancel')}}</button>
+                <button type="button" class="btn btn-danger mt-2" data-bs-dismiss="modal" id="close-button">{{translate('Cancel')}}</button>
                 <a href="" id="delete-link" class="btn btn-primary mt-2">{{translate('Delete')}}</a>
             </div>
         </div>
